4ff79d7698d32dc28230084480a24356b8116e58
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-22 12:11:16 +0200

    [feat] navbar basic html

--- src/app/core/App.jsx
+++ src/app/core/App.jsx
@@ -1,10 +1,12 @@
 import { Container } from "react-bootstrap";
 import Routing from "./Routes";
+import Navbar from "./layout/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Container fluid className="d-flex">
+        <Navbar />
         <main>
           <Routing />
         </main>
