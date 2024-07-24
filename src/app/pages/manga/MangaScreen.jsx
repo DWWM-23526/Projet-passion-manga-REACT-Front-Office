97d9f18f5d81a745c76490f81a8e8ef010fbcf53
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-24 09:35:29 +0200

    hotfix searchBar

--- src/app/pages/manga/MangaScreen.jsx
+++ src/app/pages/manga/MangaScreen.jsx
@@ -3,19 +3,37 @@ import { cardData } from "./../../../../data/cardData";
 
 import Cards from "./../../shared/components/Card/Card";
 import Header from "./../../core/layout/header/Header";
+import SearchBar from "../../shared/components/Searchbar/SearchBar";
+import { useState } from "react";
 
 const MangaScreen = () => {
-  let mangas = cardData;
+  const [filteredMangas, setFilteredMangas] = useState(cardData);
 
-  const mangaList = mangas.map((manga) => (
-    <Col xs={12} sm={6} md={4} lg={3} xl={3} className="d-flex justify-content-evenly" key={manga.id}>
-      <Cards title={manga.title} imageUrl={manga.imgSrc} url={'/manga/' + manga.id} />
+  const handleSearch = (searchTerm) => {
+    const filtered = cardData.filter((manga) =>
+      manga.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredMangas(filtered);
+  };
+
+  const mangaList = filteredMangas.map((manga) => (
+    <Col
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+      xl={3}
+      className="d-flex justify-content-evenly"
+      key={manga.id}
+    >
+      <Cards title={manga.title} imageUrl={manga.imgSrc} id={manga.id} />
     </Col>
   ));
 
   return (
     <>
       <Header title="MANGA" />
+      <SearchBar onSearch={handleSearch}/>
       <Container className="d-flex text-center">
         <Row className="g-3 mt-3">{mangaList}</Row>
       </Container>
