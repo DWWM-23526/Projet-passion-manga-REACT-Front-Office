e5c4c3fca0b99e483d76646553641931a8547063
Pierre-Marie <pierremariemarchio@gmail.com>
2024-08-02 11:11:42 +0200

    [chores] end week

--- src/app/pages/MangakaScreen.jsx
+++ src/app/pages/MangakaScreen.jsx
@@ -1,25 +1,32 @@
+import { lazy, useState } from "react";
+
+import Searchbar from "../shared/components/Searchbar/SearchBar";
 import Grid from "../shared/components/Grid/Grid";
 import defaultImg from "../../assets/img/hirohiko_araki.jpg";
-import { lazy } from "react";
 import delay from "../shared/utils/delay";
 
-const CardsPreview = lazy(() =>
-  delay(1000, import("../shared/components/Card/Card"))
-);
+const CardsPreview = lazy(() => delay(1000, import("../shared/components/Card/Card")));
 
 const MangakaScreen = () => {
   const getTitle = (item) => `${item.first_name} ${item.last_name}`;
   const getId = (item) => item.Id_mangaka;
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+
   return (
-    <Grid
-      fetchUrl="/mangaka"
-      CardComponent={CardsPreview}
-      defaultImg={defaultImg}
-      detailUrl="/details/mangaka"
-      getTitle={getTitle}
-      getId={getId}
-    />
+    <>
+      <Searchbar onSearch={setSearchTerm}/>
+      <Grid
+        searchTerm={searchTerm}
+        fetchUrl="/mangaka"
+        CardComponent={CardsPreview}
+        defaultImg={defaultImg}
+        detailUrl="/details/mangaka"
+        getTitle={getTitle}
+        getId={getId}
+      />
+    </>
   );
 };
 
